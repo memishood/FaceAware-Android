dd2886b9645c8449fca0e488b9fa67dcaa9e4667
memishood <emre.memis.49@gmail.com>
2020-07-06 01:00:51 +0300

    2.0.0

--- app/src/main/java/tr/com/emrememis/faceaware_android/MainActivity.kt
+++ app/src/main/java/tr/com/emrememis/faceaware_android/MainActivity.kt
@@ -2,28 +2,18 @@ package tr.com.emrememis.faceaware_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //faceAware.setImageResource(R.drawable.image)
 
-//        faceAware.setImageResource(R.drawable.test)
+        //faceAware.setImageDrawable(ActivityCompat.getDrawable(this,R.drawable.image))
 
-//        faceAware.setImageResource(R.drawable.test)
+        //faceAware.setImageBitmap(BitmapFactory.decodeResource(resources,R.drawable.test))
 
-//        faceAware.setImageDrawable(ActivityCompat.getDrawable(this,R.drawable.test))
-
-//        faceAware.setImageBitmap(BitmapFactory.decodeResource(resources,R.drawable.test))
-
-//        Glide.with(this).load(R.drawable.test).into(faceAware)
-
-    }
-
-    override fun onDestroy() {
-//        faceAware.onDestroy()
-        super.onDestroy()
+        //Glide.with(this).load(R.drawable.image).into(faceAware)
     }
 }
--- library/src/main/java/tr/com/emrememis/library/FaceAware.kt
+++ library/src/main/java/tr/com/emrememis/library/FaceAware.kt
@@ -5,127 +5,84 @@ import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
-import android.net.Uri
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.util.forEach
+import androidx.core.util.isNotEmpty
 import com.google.android.gms.vision.Frame
-import com.google.android.gms.vision.face.FaceDetector
-import android.util.SparseArray
 import com.google.android.gms.vision.face.Face
-import kotlin.math.roundToInt
-
+import com.google.android.gms.vision.face.FaceDetector
 
 class FaceAware constructor(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
 
-    private val faceDetector: FaceDetector = FaceDetector.Builder(context)
-        .setTrackingEnabled(false)
-        .setLandmarkType(FaceDetector.ALL_LANDMARKS)
-        .build()
-
-    private val frame: Frame.Builder? = Frame.Builder()
-
-    private var faceElevation = 10
-
-    private var isCropped = false
-
-    init {
-
-        val ta = context.obtainStyledAttributes(attrs, R.styleable.FaceAware)
-        faceElevation = ta.getInt(R.styleable.FaceAware_face_elevation,10)
-        ta.recycle()
-
-        if (faceElevation < 10) {
-            faceElevation = 10
-        }
-
-    }
-
-    override fun setImageBitmap(bm: Bitmap?) {
-        super.setImageBitmap(bm)
-        if (drawable == null) { return }
-        initializeBitmap()
-    }
-
+    private val path = Path()
+    private val rect = RectF()
 
     override fun setImageDrawable(drawable: Drawable?) {
         super.setImageDrawable(drawable)
-        if (getDrawable() == null) { return }
-        initializeBitmap()
+
+        drawable ?: return
+
+        val bitmap = when (drawable) {
+            is BitmapDrawable -> drawable.bitmap
+            is ColorDrawable -> Bitmap.createBitmap(2,2, Bitmap.Config.ARGB_8888)
+            else -> Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        }
+
+        val detector = FaceDetector.Builder(context)
+            .setTrackingEnabled(false)
+            .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+            .build()
+
+        val frame = Frame.Builder()
+            .setBitmap(bitmap)
+            .build()
+
+        val faces = detector.detect(frame)
+
+        if (faces.isNotEmpty()) {
+
+            faces.forEach { _ , value ->
+                if (value != null) {
+                    bind(value, bitmap)
+                }
+            }
+
+        }
+
+        detector.release()
+
     }
 
-    override fun setImageResource(resId: Int) {
-        super.setImageResource(resId)
-        if (drawable == null) { return }
-        initializeBitmap()
-    }
+    private fun bind(value: Face, bitmap: Bitmap) {
+        if (value.position.y + value.height <= bitmap.height
+            && value.position.x + value.width <= bitmap.width) {
 
-    override fun setImageURI(uri: Uri?) {
-        super.setImageURI(uri)
-        if (drawable == null) { return }
-        initializeBitmap()
-    }
+            val image = Bitmap.createBitmap(
+                bitmap,
+                value.position.x.toInt(),
+                value.position.y.toInt(),
+                value.width.toInt(),
+                value.height.toInt()
+            )
 
-    private fun initializeBitmap() {
-
-        if (isCropped) {
-            return
-        }
-
-        isCropped = true
-
-        val bitmap: Bitmap
-
-        when (val drawable = drawable) {
-            is BitmapDrawable -> bitmap = drawable.bitmap
-            is ColorDrawable -> bitmap = Bitmap.createBitmap(2,2, Bitmap.Config.ARGB_8888)
-            else -> bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
-        }
-
-        if (bitmap == null || frame == null) {
-            return
-        }
-
-        frame.setBitmap(bitmap)
-
-        val faces: SparseArray<Face> = faceDetector.detect(frame.build()) ?: return
-
-        if (faces.size() == 0) {
-            return
-        }
-
-        val face = faces.get(0)
-
-        var faceX = ( face.position.x - faceElevation ).roundToInt()
-        var faceY = ( face.position.y - faceElevation ).roundToInt()
-
-        if (faceY <= 0) {
-            faceY = 0
-        }
-
-        if (faceX <= 0) {
-            faceX = 0
-        }
-
-        var faceWidth = ( face.width + faceElevation * 2 ).roundToInt()
-        var faceHeight = ( face.height + faceElevation * 2 ).roundToInt()
-
-
-        if ((faceX + faceWidth) > bitmap.width) {
-            faceWidth = bitmap.width - faceX
-        }
-
-        if ((faceY + faceHeight) > bitmap.height) {
-            faceHeight = bitmap.height - faceY
-        }
-
-        try {
-            setImageBitmap(Bitmap.createBitmap(bitmap, faceX,faceY,faceWidth,faceHeight))
-        }catch (e: Exception) {
-            e.printStackTrace()
+            setImageBitmap(image)
         }
     }
 
-    fun onDestroy() {
-        faceDetector.release()
+    override fun onDraw(canvas: Canvas?) {
+        canvas ?: return
+
+        val radius = (height / 2).toFloat()
+
+        rect.left = 0f
+        rect.top = 0f
+        rect.right = width.toFloat()
+        rect.bottom = height.toFloat()
+
+        path.addRoundRect(rect, radius, radius, Path.Direction.CW)
+        canvas.clipPath(path)
+
+        super.onDraw(canvas)
     }
 }
